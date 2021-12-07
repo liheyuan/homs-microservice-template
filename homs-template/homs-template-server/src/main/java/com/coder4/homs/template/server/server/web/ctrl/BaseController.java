7d0c2c3c7d4e073a03efb195d81dbdb571a42218
lihy <lihy@yuanfudao.com>
2021-12-07 11:36:37 +0800

    FIX: it

--- homs-template/homs-template-server/src/main/java/com/coder4/homs/template/server/server/web/ctrl/BaseController.java
+++ homs-template/homs-template-server/src/main/java/com/coder4/homs/template/server/server/web/ctrl/BaseController.java
@@ -1,8 +1,8 @@
 /**
  * @(#)BaseController.java, 12月 07, 2021.
  * <p>
- * Copyright 2021 fenbi.com. All rights reserved.
- * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Copyright 2021 coder4.com. All rights reserved.
+ * CODER4.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 package com.coder4.homs.template.server.server.web.ctrl;
 
